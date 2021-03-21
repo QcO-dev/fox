398cd4852b5b2140374692f26deb118909169ea4
John Parker <johnparker.jjp@gmail.com>
2021-03-21 15:21:00 +0000

    Added built-in class <iterator> / Iterator

--- src/natives/list.c
+++ src/natives/list.c
@@ -12,7 +12,18 @@ Value listAppendNative(VM* vm, size_t argCount, Value* args, Value* bound, bool*
 	return NULL_VAL;
 }
 
+Value listIteratorNative(VM* vm, size_t argCount, Value* args, Value* bound, bool* hasError) {
+	ObjInstance* inst = newInstance(vm, vm->iteratorClass);
+
+	tableSet(vm, &inst->fields, copyString(vm, "index", 5), NUMBER_VAL(0));
+
+	tableSet(vm, &inst->fields, copyString(vm, "data", 4), *bound);
+
+	return OBJ_VAL(inst);
+}
+
 void defineListMethods(VM* vm) {
 	defineNative(vm, &vm->listMethods, "length", listLengthNative, 0, false);
 	defineNative(vm, &vm->listMethods, "append", listAppendNative, 1, false);
+	defineNative(vm, &vm->listMethods, "iterator", listIteratorNative, 0, false);
 }
--- src/natives/string.c
+++ src/natives/string.c
@@ -5,6 +5,17 @@ Value stringLengthNative(VM* vm, size_t argCount, Value* args, Value* bound, boo
 	return NUMBER_VAL(AS_STRING(*bound)->length);
 }
 
+Value stringIteratorNative(VM* vm, size_t argCount, Value* args, Value* bound, bool* hasError) {
+	ObjInstance* inst = newInstance(vm, vm->iteratorClass);
+
+	tableSet(vm, &inst->fields, copyString(vm, "index", 5), NUMBER_VAL(0));
+
+	tableSet(vm, &inst->fields, copyString(vm, "data", 4), *bound);
+
+	return OBJ_VAL(inst);
+}
+
 void defineStringMethods(VM* vm) {
 	defineNative(vm, &vm->stringMethods, "length", stringLengthNative, 0, false);
+	defineNative(vm, &vm->stringMethods, "iterator", stringIteratorNative, 0, false);
 }
